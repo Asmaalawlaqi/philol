b9a87ba61d3aac61a906478b7513a2c5ab3caa14
Asmaalawlaqi <143976152+Asmaalawlaqi@users.noreply.github.com>
2024-12-19 02:04:17 +0400

    Update sorting.c

--- asmapush/sorting.c
+++ asmapush/sorting.c
@@ -35,12 +35,12 @@ void	sort3n(t_list **stackA)
 	n2 = (*stackA)->next->content;
 	n3 = (*stackA)->next->next->content;
 	if ((n1 < n2) && (n2 < n3) && (n1 < n3))
-		return ;
+		return ; //: Simple swap if needed.
 	if ((n1 > n2) && (n2 < n3) && (n1 < n3))
-		sa(stackA);
+		sa(stackA); //se specific conditions to swap and rotate elements into the correct order.
 	else if ((n1 > n2) && (n2 > n3) && (n1 > n3))
 	{
-		sa(stackA);
+		sa(stackA);//Move the minimum element to stackB, sort the remaining 3 elements, and move the minimum element back.
 		rra(stackA);
 	}
 	else if ((n1 < n2) && (n2 > n3) && (n1 > n3))
@@ -84,3 +84,14 @@ void	sort5n(t_list **stackA, t_list **stackB)
 	sort4n(stackA, stackB);
 	pa(stackB, stackA);
 }
+
+
+//2 elements: Simple swap if needed.
+///3 elements: Use specific conditions to swap and rotate elements into the correct order.
+//4 elements: Move the minimum element to stackB, sort the remaining 3 elements, and move the minimum element back.
+//5 elements: Move the minimum element to stackB, sort the remaining 4 elements, and move the minimum element back.
+//more than 5 ; Find the minimum element and move it to the top of the stack.
+// Push the minimum element to stackB.
+// Sort the remaining 4 elements in stackA using sort4n.
+// Push the minimum element back into stackA after sorting.
+
